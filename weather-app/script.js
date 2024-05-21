ea388a33a2cadca46b4b3656fde87e90ef1c8c02
gozica <109421350+gozica@users.noreply.github.com>
2024-05-21 18:12:09 -0400

    updated js

--- weather-app/script.js
+++ weather-app/script.js
@@ -8,9 +8,12 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  const data = await response.json();
 
-  console.log(data);
+  if(response.status == 404){
+    document.querySelector(".error").style.display = "block";
+     document.querySelector(".weather").style.display = "none";
+  }else{
+  const data = await response.json();
 
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°f";
@@ -19,21 +22,31 @@ async function checkWeather(city) {
   document.querySelector(".wind").innerHTML =
     Math.round(data.wind.speed) + " mph";
 
-  if (data.weather[0].main == "Clouds") {
-    weatherIcon.src = "images/clouds.png";
-  } else if (data.weather[0].main == "Clear") {
-    weatherIcon.src = "images/clear.png";
-  } else if (data.weather[0].main == "Rain") {
-    weatherIcon.src = "wimages/rain.png";
-  } else if (data.weather[0].main == "Drizzle") {
-    weatherIcon.src = "images/drizzle.png";
-  } else if (data.weather[0].main == "Mist") {
-    weatherIcon.src = "images/mist.png";
+
+
+
+
+     if (data.weather[0].main == "Clouds") {
+       weatherIcon.src = "images/clouds.png";
+     } else if (data.weather[0].main == "Clear") {
+       weatherIcon.src = "images/clear.png";
+     } else if (data.weather[0].main == "Rain") {
+       weatherIcon.src = "wimages/rain.png";
+     } else if (data.weather[0].main == "Drizzle") {
+       weatherIcon.src = "images/drizzle.png";
+     } else if (data.weather[0].main == "Mist") {
+       weatherIcon.src = "images/mist.png";
+     }
+
+     document.querySelector(".weather").style.display = "block";
+     document.querySelector(".error").style.display = "none";
+
   }
+
+
 }
 
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
 
-checkWeather();
