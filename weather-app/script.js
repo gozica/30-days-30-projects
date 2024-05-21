488dc4f364747b18ef5c2ebd9f8b683dd7b1ef1a
gozica <109421350+gozica@users.noreply.github.com>
2024-05-21 17:54:06 -0400

    added js

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,3 +1,39 @@
 const apiKey = "e049244c47b06da6db12af4f0ec42242";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=imperial";
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  const data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°f";
+  document.querySelector(".humidity").innerHTML =
+    Math.round(data.main.humidity) + "%";
+  document.querySelector(".wind").innerHTML =
+    Math.round(data.wind.speed) + " mph";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "wimages/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+checkWeather();
